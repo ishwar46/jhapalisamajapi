71744729c2a92685df63e449bcaaf34ce026f800
SushantPant <sushantpant34@gmail.com>
2025-02-17 12:08:51 +0545

    Add Backend For Scholarship
    Added backend for scholarship

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require("express")
-const cors = require("cors")
-const env = require("dotenv")
-const connectToDatabase = require("./database/db")
-const fs = require("fs")
-const path = require("path")
-
+const express = require("express");
+const cors = require("cors");
+const env = require("dotenv");
+const connectToDatabase = require("./database/db");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -14,46 +13,51 @@ env.config();
 connectToDatabase();
 
 const corsOptions = {
-    origin: true,
-    credentials: true,
-    optionSuccessStatus: 200
-}
+  origin: true,
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 // Serve static files from the uploads folder
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //Routes
-app.use("/api/users", require("./routes/userRoutes"))
-app.use("/api/admin", require("./routes/adminRoutes"))
-app.use('/api/uploads', require('./routes/uploadRoutes'));
-app.use('/api/menu-items', require('./routes/menuRoutes'));
-app.use('/api/sidebar', require('./routes/sidebarRoutes'))
-app.use('/api/hero-slides', require('./routes/heroSlideRoutes'));
-app.use('/api/gallery', require('./routes/galleryRoutes'));
-app.use('/api/programs', require('./routes/programsRoutes'));
-app.use('/api/testimonials', require('./routes/testimonialsRoutes'));
-app.use('/api/blogs', require('./routes/blogRoutes'));
-app.use('/api/contact', require('./routes/contactRoutes'));
-app.use('/api/mission-story', require('./routes/missionStroryRoutes'));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/admin", require("./routes/adminRoutes"));
+app.use("/api/uploads", require("./routes/uploadRoutes"));
+app.use("/api/menu-items", require("./routes/menuRoutes"));
+app.use("/api/sidebar", require("./routes/sidebarRoutes"));
+app.use("/api/hero-slides", require("./routes/heroSlideRoutes"));
+app.use("/api/gallery", require("./routes/galleryRoutes"));
+app.use("/api/programs", require("./routes/programsRoutes"));
+app.use("/api/testimonials", require("./routes/testimonialsRoutes"));
+app.use("/api/blogs", require("./routes/blogRoutes"));
+app.use("/api/contact", require("./routes/contactRoutes"));
+app.use("/api/mission-story", require("./routes/missionStroryRoutes"));
 app.use("/api/stories", require("./routes/storyRoutes"));
 app.use("/api/scholarships", require("./routes/scholarshipRoutes"));
-app.use("/api/scholarship-recipients", require("./routes/scholarshipRecipientsRoutes"));
+app.use(
+  "/api/scholarship-recipients",
+  require("./routes/scholarshipRecipientsRoutes")
+);
 app.use("/api/hearse-vehicles", require("./routes/hearseVehicleRoutes"));
-app.use("/api/executive-committee", require("./routes/executiveCommitteeRoutes"));
+app.use(
+  "/api/executive-committee",
+  require("./routes/executiveCommitteeRoutes")
+);
 app.use("/api/annual-donations", require("./routes/annualDonationRoutes"));
+app.use("/api/onetime-donations", require("./routes/onetimeDonationRoutes"));
 app.use("/api/dmv-chapter", require("./routes/dmvChapterRoutes"));
 app.use("/api/texas-chapter", require("./routes/texasChapterRoutes"));
 app.use("/api/documents", require("./routes/documentRoutes"));
 
+app.get("/", (req, res) => {
+  res.send("Hello!! This is Jhapali Samaja USA");
+});
 
-
-app.get('/', (req, res) => {
-    res.send("Hello!! This is Jhapali Samaja USA")
-})
-
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server is Running on PORT ${PORT}`)
-})
+  console.log(`Server is Running on PORT ${PORT}`);
+});
