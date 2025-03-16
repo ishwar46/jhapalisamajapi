f49a5cb220f612138109ddb9d8a8bf820a777054
Ishwar Chaudhary <ishwarura@gmail.com>
2025-03-16 16:33:50 +0545

    Update donationRoutes.js

--- routes/donationRoutes.js
+++ routes/donationRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Donation = require("../models/Donation");
 const router = express.Router();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -17,6 +18,24 @@ router.post("/donate", async (req, res) => {
     } catch (err) {
         console.error("Error creating PaymentIntent:", err);
         res.status(500).json({ error: err.message });
+    }
+});
+
+router.get("/donate", async (req, res) => {
+    try {
+        let page = await Donation.find().populate("userId", "fullName email");
+        if (!page) {
+            page = new Donation();
+            await page.save();
+            page = await Donation.find().populate("userId", "fullName email");
+        }
+        return res.status(200).json(page)
+            ;
+    } catch (error) {
+        console.error("getDonationPage Error:", error);
+        return res
+            .status(500)
+            .json({ error: "Server error fetching annual donation page." });
     }
 });
 
