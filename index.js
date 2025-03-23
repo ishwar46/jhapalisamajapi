602a949f0cd7af07de40aaba408040cff2d1f901
Ishwar Chaudhary <ishwarura@gmail.com>
2025-03-23 17:35:09 +0545

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
   origin: true,
   origin: ["https://jhapali.org", "https://www.jhapali.org"],
-  // credentials: true,
+  credentials: true,
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
